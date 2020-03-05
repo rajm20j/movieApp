08f15ce51d8ba1974c1776361b27af6817dc1c3e
Mayank <rajm20j@gmail.com>
2020-03-05 10:58:12 +0530

    Nested recycler view working almost as intended

--- app/src/main/java/com/example/themoviesdb/allShows/model/ListAllShowsGridAdapter.kt
+++ app/src/main/java/com/example/themoviesdb/allShows/model/ListAllShowsGridAdapter.kt
@@ -2,11 +2,15 @@ package com.example.themoviesdb.allShows.model
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.annotation.Nullable
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.themoviesdb.R
@@ -39,6 +43,62 @@ class ListAllShowsGridAdapter() : RecyclerView.Adapter<ListAllShowsGridAdapter.V
             Utils.currentShowTitle = listPositionItem.title.toString()
             intent.putExtra("tvId", listPositionItem.tvId)
             context.startActivity(intent)
+        }
+    }
+
+    fun submitList(newList: MutableList<GridItemModel>)
+    {
+//        Log.v("TEST2", "submitList")
+        val oldList = listItems
+        val diffResult: DiffUtil.DiffResult = DiffUtil.calculateDiff(
+            ReadItemDiffUtil(oldList, newList)
+        )
+//        listItems.clear()
+        listItems = newList
+        for(items in listItems)
+            Log.v("TEST2", items.title.toString())
+
+        diffResult.dispatchUpdatesTo(this)
+    }
+
+    class ReadItemDiffUtil(
+        var oldList: MutableList<GridItemModel>,
+        var newList: MutableList<GridItemModel>
+    ): DiffUtil.Callback() {
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+//            Log.v("TEST2", "areItemsTheSame")
+            return (oldList[oldItemPosition].tvId == newList[newItemPosition].tvId)
+        }
+
+        override fun getOldListSize(): Int {
+//            Log.v("TEST2", "getOldListSize")
+            return oldList.size
+        }
+
+        override fun getNewListSize(): Int {
+//            Log.v("TEST2", "getNewListSize")
+            return newList.size
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+//            Log.v("TEST2", "areContentsTheSame")
+            if(oldList[oldItemPosition].tvId != newList[newItemPosition].tvId)
+                return false
+            if(oldList[oldItemPosition].posterPath != newList[newItemPosition].posterPath)
+                return false
+            if(oldList[oldItemPosition].backdropPath != newList[newItemPosition].backdropPath)
+                return false
+            if(oldList[oldItemPosition].overview != newList[newItemPosition].overview)
+                return false
+            if(oldList[oldItemPosition].title != newList[newItemPosition].title)
+                return false
+//            Log.v("TEST2", "areContentsTheSame is true")
+            return true
+        }
+
+        @Nullable
+        override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? {
+            return super.getChangePayload(oldItemPosition, newItemPosition)
         }
     }
 
--- app/src/main/java/com/example/themoviesdb/readEpisodeStories/model/ReadGridItemAdapter.kt
+++ app/src/main/java/com/example/themoviesdb/readEpisodeStories/model/ReadGridItemAdapter.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.themoviesdb.R
 import com.example.themoviesdb.readEpisodeStories.ReadEpisodeStoriesVM
